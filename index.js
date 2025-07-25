977db8bd62aa568b02b72f5d77d3d187df64fa5a
Samir Kayamkhani <134730963+Samir-kyamkhani@users.noreply.github.com>
2025-07-25 10:00:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ dotenv.config({ path: "./.env" });
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.get("/", (req, res) => {
   res.send(`Hello World! ============= ${PORT}`);
 });
 
-app.listen(PORT, () => {
+
+
+app.listen(PORT, HOST, () => {
   console.log(`SERVER RUNNING ON http://localhost:${PORT}`);
 });
